4318cd9e38b11a466acdb28a730a24136a7e1c3d
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-05-09 17:30:04 +0530

    clean branch

--- src/app.js
+++ src/app.js
@@ -1,31 +1,7 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-// 1. first letter should be always capital
-// 2. return single element from the component
-// 3. apply inline style as object and propties should be in camelCase
-// 4. instead of class use className
-function App({ title, desc, number }) {
-  return (
-    <>
-      <h1>{title}</h1>
-      <h2>{desc}</h2>
-      <h3>{number}</h3>
-      <input type="checkbox" className="border border-gray-300 rounded-lg" />
-      <div />
-      <div />
-    </>
-  )
-}
-
-App.propTypes = {
-  title: PropTypes.string.isRequired,
-  desc: PropTypes.string,
-  number: PropTypes.number.isRequired,
-}
-
-App.defaultProps = {
-  desc: 'Batsman',
+function App() {
+  return <div>App</div>
 }
 
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,44 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
-// import App from './app'
-import User from './user'
-import Test from './test'
+import App from './app'
 
 const container = document.getElementById('app')
 const root = createRoot(container)
 
-// const bgColor = 'red'
-// const color = '#fff'
-
-class App extends Component {
-  state = {
-    count: 5,
-  }
-
-  changeCount = () => {
-    this.setState({ count: 10 })
-  }
-
-  render() {
-    const { count } = this.state
-    return (
-      <div>
-        <Test count={count} name="yagnesh" />
-        <button onClick={this.changeCount} type="button">
-          Change Count
-        </button>
-      </div>
-    )
-  }
-}
-
-root.render(
-  <>
-    {/* <App title="Virat Kohli" number={10} />
-    <App title="Rohit Sharma" desc="Opening Batsman" number={45} />
-    <User firstName="Yagnesh" lastName="Modh" age={36} gender="male" /> */}
-    <User firstName="Rohit" lastName="Sharma" age={32} gender="male" />
-    <App />
-  </>
-)
+root.render(<App />)
